e61a5d99871f836aedba7d38c0e1276d995df707
Santiago Lucero <61132148+santilucero1@users.noreply.github.com>
2024-07-17 15:37:13 -0300

    Add files via upload
    New animations , websites, and header working

--- javascript.js
+++ javascript.js
@@ -102,3 +102,28 @@ for(const star of document.getElementsByClassName("magic-star")) {
     setInterval(() => animate(star), 1000);
   }, index++ * (interval / 3))
 }
+
+
+
+//Reveal animation
+
+document.addEventListener('DOMContentLoaded', function() {
+  const revealElements = document.querySelectorAll('.reveal');
+
+  function revealOnScroll() {
+      const windowHeight = window.innerHeight;
+
+      revealElements.forEach(element => {
+          const elementTop = element.getBoundingClientRect().top;
+
+          if (elementTop < windowHeight - 100) {
+              element.classList.add('visible');
+          } else {
+              element.classList.remove('visible');
+          }
+      });
+  }
+
+  window.addEventListener('scroll', revealOnScroll);
+  revealOnScroll(); // Ejecuta al cargar la página para los elementos visibles al inicio
+});
